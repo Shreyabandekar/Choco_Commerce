094f71e43c4b6fca6671e295428021e3d928e818
Shreyabandekar <124146602+Shreyabandekar@users.noreply.github.com>
2025-06-17 22:35:52 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { StoreProvider } from "./utils/GlobalState";
+
+import MainContiner from "./components/MainContainer";
+import SingleProduct from "./components/SingleProduct";
+import NavTabs from "./components/NavTabs";
+import Login from "./components/pages/Login";
+import SignUp from "./components/pages/Signup";
+import Cart from "./components/pages/Cart/Cart";
+import RouteChangeTracker from "./components/RouteChangeTracker";
+import ReactGA from "react-ga";
+
+const TRACKING_ID = "G-ST37X3SNG0"; // YOUR_OWN_TRACKING_ID or store in env
+ReactGA.initialize(TRACKING_ID);
+
+const App = () => {
+  return (
+    <Router>
+      <StoreProvider>
+        <NavTabs />
+        <Routes>
+          <Route path="/" element={<MainContiner />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/products/:id" element={<SingleProduct />} />
+        </Routes>
+        <RouteChangeTracker />
+      </StoreProvider>
+    </Router>
+  );
+};
+
+export default App;
